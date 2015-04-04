485b2348178a7d952543a78eda1ab25b550ef70a
Dan Devine <dan@blackroad.ie>
2015-04-04 12:00:12 +0100

    shortcode button for form

--- js/BRS_new-button.js
+++ js/BRS_new-button.js
@@ -1,1 +1,12 @@
 
+(function() {
+	tinymce.PluginManager.add('my_mce_button', function( editor, url ) {
+		editor.addButton('my_mce_button', {
+			text: 'ADD FORM',
+			icon: false,
+			onclick: function() {
+				editor.insertContent('[BRS_contact-form]');
+			}
+		});
+	});
+})();
