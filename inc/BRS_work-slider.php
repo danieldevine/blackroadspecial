ef6ea61127e090c00a173f3b05a0b5579136cae8
Dan Devine <dan@blackroad.ie>
2015-04-23 09:52:53 +0100

    tidy

--- inc/BRS_work-slider.php
+++ inc/BRS_work-slider.php
@@ -1,31 +1,11 @@
 <div class="liquid-slider" id="work-slider">
 
-    <?php //Ok, let's set up a WP Query loop to grab some posts from a particular post type and dump them into the slider.
+  <div><!--start slide-->
+    <div><?php the_title(); ?></div>
+  </div><!--//end slide -->
 
-      $args=array(
-        'posts_per_page'=>'2',
-      );
+  <div><!--start slide-->
+    <div>  <?php the_post_thumbnail(); ?></div>
+  </div><!--//end slide -->
 
-      $category_posts = new WP_Query($args);
-
-      if($category_posts->have_posts()) :
-
-      while($category_posts->have_posts()) :
-
-      $category_posts->the_post();
-
-    ?>
-
-     <div><!--start of the slide-->
-
-
-   <div class="slider-excerpt"><?php the_excerpt(); ?></div>
-
-
-     </div><!--end of the slide -->
-     <?php endwhile; else: ?>
-      I'm afraid that there are no posts.
-     <?php endif; ?>
-
-
-</div>
+</div><!--//end slider -->
